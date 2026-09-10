546cd3f4c3c7df24b58071f97bc49350377aeadd
gatorsindre <olsensindre51@gmail.com>
2026-09-10 23:54:54 +0200

    added sprinting

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,12 +1,14 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float walkingSpeed = 5;
+    float walkingSpeed = 8;
     public Rigidbody2D rb;
-    public ParticleSystem sprintParticles;
-
+    ParticleSystem sprintParticles;
+    public GameObject sprintParticle;
+    
     public InputAction MoveAction;
     public InputAction SprintAction;
     void OnEnable()
@@ -19,10 +21,11 @@ public class Movement : MonoBehaviour
         MoveAction.Disable();
         SprintAction.Disable();
     }
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprintParticles = sprintParticle.GetComponent<ParticleSystem>();
     }
     void Update()
     {
@@ -34,13 +37,37 @@ public class Movement : MonoBehaviour
         {
             sprintParticles.Stop();
         }
+
+        Vector2 movement_left_right = MoveAction.ReadValue<Vector2>();
+
+        if (SprintAction.IsPressed())
+        {
+            walkingSpeed = 14;
+
+            if (movement_left_right[0] > 0)
+            {
+                Vector3 currentEuler = transform.eulerAngles;
+                currentEuler.x = -6f;
+                currentEuler.y = -90f;
+                currentEuler.z = 90f;
+                sprintParticle.transform.rotation = Quaternion.Euler(currentEuler);
+            }
+            else if (movement_left_right[0] < 0)
+            {
+                Vector3 currentEuler = transform.eulerAngles;
+                currentEuler.x = -174f;
+                currentEuler.y = -90f;
+                currentEuler.z = 90f;
+                sprintParticle.transform.rotation = Quaternion.Euler(currentEuler);
+            }
+        } else
+            walkingSpeed = 8;
     }
     void FixedUpdate()
     {
         Vector2 movement = MoveAction.ReadValue<Vector2>();
 
-        if (movement.magnitude > 1)
-            movement.Normalize();
+        movement = Vector2.ClampMagnitude(movement, 1);
 
         rb.linearVelocity = movement * walkingSpeed;
     }
